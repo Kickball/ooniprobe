8b2f8ace63cd88f7a738796b20e9a2e09e957a11
Edward <ed.dickson@hotmail.com>
2022-10-18 10:50:05 +0000

    chore: best practice changes for release and build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 podman build --build-arg OONIPROBE_VERSION=3.16.3 --build-arg OONIPROBE_UPLOAD_RESULTS=true -t ooniprobe .
--- release.sh
+++ release.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Gather official OONI Probe versions.
 # (1) Get project releases via GitHub API. (2) Filter the tag JSON data to only the tag name. (3) Use RegEx to filter tag names to only the version numbers (no letters) and remove any beta/alpha releases (those end with -alpha/-beta). (4) Return the results of the RegEx as an array. (5) Merge the arrays, source: https://stackoverflow.com/a/63908503. (6) Remove punctuation to enable conversion to bash array, source https://stackoverflow.com/a/61299548.
 probe_versions=($(curl -H "Accept: application/vnd.github+json" https://api.github.com/repos/ooni/probe-cli/releases?per_page=100 | jq '.[]."tag_name" | match("([1234567890])+\\.([1234567890])+\\.([1234567890])*$"; "g") | [.string]' | jq -rs 'reduce .[] as $x ([]; . + $x)' | tr -d '[]," '))
@@ -12,14 +14,15 @@ echo "Container Versions:"
 echo "${container_versions[@]}"
 
 # Identify versions of the OONI Probe software that do not have equivilant container image versions. Source: https://stackoverflow.com/a/28161520.
-missing_versions=($(echo ${probe_versions[@]} ${container_versions[@]} ${container_versions[@]} | tr ' ' '\n' | sort | uniq -u | sort -V))
+missing_versions=($(echo "${probe_versions[@]}" "${container_versions[@]}" "${container_versions[@]}" | tr ' ' '\n' | sort | uniq -u | sort -V))
 
 echo "Missing Versions:"
 echo "${missing_versions[@]}"
 
 # TBC for actual process, will likely involve Git branches/tags and GitHub actions to build the images.
-for version in $missing_versions; 
+for version in "${missing_versions[@]}"
 do
-    docker build --build-arg OONIPROBE_VERSION=$version --build-arg OONIPROBE_UPLOAD_RESULTS=true -t ooniprobe:$version kickball/ooniprobe:$version . 
-    docker push kickball/ooniprobe:$version
+    git tag "$version"
+    #docker build --build-arg OONIPROBE_VERSION="$version" --build-arg OONIPROBE_UPLOAD_RESULTS=true -t ooniprobe:"$version" kickball/ooniprobe:"$version" . 
+    #docker push kickball/ooniprobe:"$version"
 done
